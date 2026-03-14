4027de57ff46f0c122453aacfcfc449fc9dfe15a
Frostnox <51527640+Frostnox@users.noreply.github.com>
2026-03-14 15:40:24 -0700

    Melted metal alloying

--- src/main/java/frostnox/nightfall/block/block/meltedmetal/MeltedMetalBlockEntity.java
+++ src/main/java/frostnox/nightfall/block/block/meltedmetal/MeltedMetalBlockEntity.java
@@ -10,10 +10,9 @@ import frostnox.nightfall.registry.forge.BlockEntitiesNF;
 import frostnox.nightfall.registry.forge.BlocksNF;
 import frostnox.nightfall.registry.forge.FluidsNF;
 import frostnox.nightfall.util.LevelUtil;
+import frostnox.nightfall.util.data.Vec2f;
 import frostnox.nightfall.util.math.AxisDirection;
-import it.unimi.dsi.fastutil.objects.ObjectArrayFIFOQueue;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import it.unimi.dsi.fastutil.objects.ObjectArraySet;
+import it.unimi.dsi.fastutil.objects.*;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
@@ -25,12 +24,13 @@ import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class MeltedMetalBlockEntity extends BlockEntity {
     public float temperature = TieredHeat.ORANGE.getUpperTemp(), targetTemperature;
     public BlockState originalState = BlocksNF.METAL_BLOCKS.get(Metal.COPPER).get().defaultBlockState();
     public IMetal.Entry metal = RegistriesNF.getMetals().getValue(ResourceLocation.fromNamespaceAndPath(Nightfall.MODID, Metal.COPPER.getName()));
-    public int units = 400;
+    public int units = 400, alloyTimer = 20 * 5;
     public boolean hasSlag = false, untouched = true;
 
     public MeltedMetalBlockEntity(BlockPos pPos, BlockState pBlockState) {
@@ -71,10 +71,67 @@ public class MeltedMetalBlockEntity extends BlockEntity {
             MeltedMetalBlockEntity metal = metals.get(i);
             metal.units = unitsMap[i];
             if(metal.units == 0) {
-                metal.level.removeBlock(metal.getBlockPos(), false);
+                if(metal.hasSlag) metal.level.setBlockAndUpdate(metal.getBlockPos(), BlocksNF.SLAG.get().defaultBlockState());
+                else metal.level.removeBlock(metal.getBlockPos(), false);
             }
             metal.untouched = false;
+            metal.alloyTimer = 20 * 5;
             metal.setChanged();
+        }
+    }
+
+    public void alloy() {
+        List<MeltedMetalBlockEntity> entities = new ObjectArrayList<>(LevelUtil.MAX_BLAST_FURNACE_AREA);
+        Map<IMetal, Integer> unitsMap = new Object2IntArrayMap<>(4);
+        ObjectArrayFIFOQueue<BlockPos> positions = new ObjectArrayFIFOQueue<>(32);
+        positions.enqueue(getBlockPos());
+        ObjectArraySet<BlockPos> visited = new ObjectArraySet<>();
+        while(!positions.isEmpty() && entities.size() < 64) {
+            BlockPos pos = positions.dequeue();
+            if(visited.contains(pos)) continue;
+            else visited.add(pos);
+            if(level.getBlockEntity(pos) instanceof MeltedMetalBlockEntity entity) {
+                entities.add(entity);
+                if(!unitsMap.containsKey(entity.metal.value)) unitsMap.put(entity.metal.value, entity.units);
+                else unitsMap.put(entity.metal.value, entity.units + unitsMap.get(entity.metal.value));
+                for(AxisDirection dir : AxisDirection.XZ) positions.enqueue(pos.offset(dir.x, dir.y, dir.z));
+            }
+        }
+        int totalUnits = 0;
+        for(int units : unitsMap.values()) totalUnits += units;
+
+        IMetal.Entry alloyMetal = null;
+        for(IMetal.Entry metalType : RegistriesNF.getMetals()) {
+            Map<IMetal, Vec2f> bases = metalType.value.getBaseMetals();
+            if(unitsMap.size() != bases.size()) continue;
+            boolean alloyed = true;
+            for(IMetal key : unitsMap.keySet()) {
+                if(!bases.containsKey(key)) {
+                    alloyed = false;
+                    break;
+                }
+                float part = ((float) unitsMap.get(key)) / totalUnits;
+                Vec2f range = bases.get(key);
+                if(part < range.x() || part > range.y()) {
+                    alloyed = false;
+                    break;
+                }
+            }
+            if(alloyed) {
+                alloyMetal = metalType;
+                break;
+            }
+        }
+
+        if(alloyMetal != null) for(MeltedMetalBlockEntity entity : entities) {
+            entity.metal = alloyMetal;
+            entity.untouched = false;
+            entity.alloyTimer = 0;
+            entity.setChanged();
+        }
+        else for(MeltedMetalBlockEntity entity : entities) { //Stop other metals in this pool from trying to alloy again without any changes
+            entity.alloyTimer = 0;
+            entity.setChanged();
         }
     }
 
@@ -97,8 +154,12 @@ public class MeltedMetalBlockEntity extends BlockEntity {
             else {
                 TieredHeat heat = TieredHeat.fromTemp(entity.temperature);
                 if(heat.getTier() != state.getValue(MeltedMetalBlock.HEAT)) level.setBlockAndUpdate(pos, state.setValue(MeltedMetalBlock.HEAT, heat.getTier()));
+                if(entity.alloyTimer > 0) {
+                    if(entity.alloyTimer == 1) entity.alloy();
+                    else entity.alloyTimer--;
+                }
+                entity.setChanged();
             }
-            entity.setChanged();
         }
     }
 
@@ -110,6 +171,7 @@ public class MeltedMetalBlockEntity extends BlockEntity {
         originalState = MeltedMetalBlock.stateById(tag.getInt("originalState"));
         metal = RegistriesNF.getMetals().getValue(ResourceLocation.parse(tag.getString("metal")));
         units = tag.getInt("units");
+        alloyTimer = tag.getInt("alloyTimer");
         hasSlag = tag.getBoolean("hasSlag");
         untouched = tag.getBoolean("untouched");
     }
@@ -122,6 +184,7 @@ public class MeltedMetalBlockEntity extends BlockEntity {
         tag.putInt("originalState", MeltedMetalBlock.getId(originalState));
         tag.putString("metal", metal.getRegistryName().toString());
         tag.putInt("units", units);
+        tag.putInt("alloyTimer", alloyTimer);
         tag.putBoolean("hasSlag", hasSlag);
         tag.putBoolean("untouched", untouched);
     }
